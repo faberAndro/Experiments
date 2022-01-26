e09ac8c0ff052aa0b7e697d191f5452a1e051e07
FabIT <40968674+faberAndro@users.noreply.github.com>
2022-01-26 22:53:04 +0000

    finance: completed time-series manipulation: ready now to extract features

--- financial_analysis/stock_manipulator_V1.py
+++ financial_analysis/stock_manipulator_V1.py
@@ -12,8 +12,6 @@ from scipy import stats
 from stock_loader import load_equity
 import stock_manipulator_V0 as Smo
 
-plot_regressions = True
-
 
 # ALTERNATIVE: use pandas rolling functions
 def moving_average(xa, order):
@@ -37,6 +35,31 @@ def compute_regr(m: int, mm: np.array, y: np.array) -> (np.array, np.array):
     value_chunk = y[mm[m]: mm[m+1] + 1]
     p0, p1 = regression_edges(f=value_chunk)
     return np.array([p0[0] + mm[m], p0[1]]), np.array([p1[0] + mm[m], p1[1]])
+
+
+def plot_regression(r_array: list):
+    for p in r_array:
+        plt.plot((p[0][0], p[1][0]), (p[0][1], p[1][1]), c='red')
+
+
+def compute_regression_extrema_directions(r):
+    directions = []
+    for points in r:
+        x1, y1, x2, y2 = points[0][0], points[0][1], points[1][0], points[1][1]
+        directions.append(np.sign((y2 - y1)/(x2 - x1)).astype(int))
+    return directions
+
+
+def zig_zag_test(y_test_directions):
+    """
+
+    :param y_test_directions:
+    :return: True if the sequence of directions is a perfect zigzag
+    """
+    shifted_directions = np.roll(y_test_directions, -1)
+    test = (y_test_directions + shifted_directions)[:-1]
+    monotone_indexes = np.where(test != 0) + np.array(1)
+    return not monotone_indexes.any()
 
 
 if __name__ == '__main__':
@@ -75,25 +98,37 @@ if __name__ == '__main__':
     x_mm = np.sort(np.concatenate((np.array([ORDER-1]), better_minima, better_maxima)), axis=0)     # reordering maxima and minima:
     y_mm = values.take(x_mm)
 
-    # CONNECTING BETTER MAXIMA AND MINIMA THROUGH REGRESSION LINES
+    # REMOVING THE POINTS LESS THAN 'ORDER/2' CLOSE (EXAMPLE: IF ORDER=14, REMOVE POINTS THAT ARE LESS THAN 7 DAYS CLOSER TO THE PREVIOUS ONES)
+    neighbourhood = int(ORDER/2)
+    close_positions = np.where(np.diff(x_mm) <= 7) + np.array(1)
+    x_distant = np.delete(x_mm, close_positions)
+    y_distant = np.delete(y_mm, close_positions)
+
+    # ENSURING THERE IS A PERFECT ALTERNANCE IN UPHILLS AND DOWNHILLS
+    # todo: x and y can be vectorised to a single array
+    y_mm_directions = np.sign(np.diff(y_distant))
+    shifted_array = np.roll(y_mm_directions, -1)
+    zig_zag_test = (y_mm_directions + shifted_array)[:-1]           # the sum of N.2 consecutive y directions should always be 0, so in theory this array should be made only of zeros
+    monotone_spots = np.where(zig_zag_test != 0) + np.array(1)      # these are the indexes (in natural numbers) of the monotone points
+    zigzag_x_mm = np.delete(x_distant, monotone_spots)
+    zigzag_y_mm = np.delete(y_distant, monotone_spots)
+
+    # CONNECTING BETTER MAXIMA AND MINIMA, NOW ALSO OPTIMISED, THROUGH REGRESSION LINES
     # todo: get rid of oscillations shorted than 1 week, amending the better max and min arrays
     #  doing that will aslo ensure the  high-variability but static-on-the-average intervals captured can be still used for investment
     regression_extrema = []
-    for m in range(len(x_mm) - 1):
-        regression_extrema.append(compute_regr(m, x_mm, values))
-
-    def plot_regression(r_array: list):
-        for p in r_array:
-            plt.plot((p[0][0], p[1][0]), (p[0][1], p[1][1]), c='red')
+    for m in range(len(zigzag_x_mm) - 1):
+        regression_extrema.append(compute_regr(m, zigzag_x_mm, values))
 
     # PLOTTING THE RESULTS
     plt.plot(values, label='equity', c='blue', linestyle='--')
     plt.plot(y_ma_2b, c='red', label='moving av 2b')
-    plt.scatter(maxima, y_max, s=50, edgecolor='red', facecolor='none')
     plt.scatter(minima, y_min, s=50, edgecolor='black', facecolor='none')
+    plt.scatter(maxima, y_max, s=50, edgecolor='red', facecolor='none')
     plt.scatter(better_minima, better_y_min, s=50, edgecolor='none', facecolor='green')
     plt.scatter(better_maxima, better_y_max, s=50, edgecolor='none', facecolor='grey')
     plt.plot(x_mm, y_mm, c='green')
+    plt.plot(zigzag_x_mm, zigzag_y_mm, c='orange')
     plot_regression(regression_extrema)
     plt.grid(True)
     plt.legend()
