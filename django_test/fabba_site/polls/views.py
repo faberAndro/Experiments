114538c9247b80cde530d9a736262f4b68be4656
FabIT <40968674+faberAndro@users.noreply.github.com>
2022-03-01 11:58:53 +0000

    added templates to Django

--- django_test/fabba_site/polls/views.py
+++ django_test/fabba_site/polls/views.py
@@ -1,14 +1,23 @@
-from django.shortcuts import render
+from django.http import Http404
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from .models import Question
+from django.template import loader
+
+# Leave the rest of the views (detail, results, vote) unchanged
 
 
 def index(request):
-    return HttpResponse("Hi. I'm Fab. You are viewing the polls index")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return render(request, 'polls/index.html', context)
 
 
 def detail(request, question_id):
-    response = f"You are looking at question {question_id}"
-    return HttpResponse(response)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
 
 def results(request, question_id):
