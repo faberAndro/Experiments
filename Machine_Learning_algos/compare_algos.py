2059e139fe88010780f7ee113e775b63b12015be
FabIT <40968674+faberAndro@users.noreply.github.com>
2022-04-14 13:23:26 +0100

    refactored SVM function and added comparison dataframe

--- Machine_Learning_algos/compare_algos.py
+++ Machine_Learning_algos/compare_algos.py
@@ -1,29 +1,31 @@
 import graphviz
 import numpy as np
-import pandas as pd
-from matplotlib import pyplot as plt
-
-from sklearn import datasets
+from sklearn import datasets, svm, tree
 from sklearn.model_selection import train_test_split
-
 from sklearn.naive_bayes import GaussianNB, MultinomialNB, BernoulliNB, ComplementNB
-from sklearn import tree
-from sklearn import svm
 from sklearn.utils import Bunch
 
-DATASET_NAMES = ['wine', 'iris']
+DATASET_NAMES = ['wine', 'iris', 'digits', 'diabetes', 'breast cancer', 'linnerud']
 
 
-def visualise_sample_dataset(dataset_name: str):
+def load_sample_dataset(dataset_name: str) -> Bunch:
     dataset = {}
     if dataset_name == 'wine':
         dataset = datasets.load_wine(as_frame=True)
     elif dataset_name == 'iris':
         dataset = datasets.load_iris(as_frame=True)
+    elif dataset_name == 'digits':
+        dataset = datasets.load_digits(as_frame=True)
+    elif dataset_name == 'brest cancer':
+        dataset = datasets.load_breast_cancer(as_frame=True)
+    elif dataset_name == 'diabetes':
+        dataset = datasets.load_diabetes(as_frame=True)
+    elif dataset_name == 'linnerud':
+        dataset = datasets.load_linnerud(as_frame=True)
     return dataset
 
 
-def prepare_trainig_data(dataset, test_size: float = 0.3):
+def prepare_trainig_data(dataset, test_size: float = 0.3) -> tuple:
     training_data = train_test_split(dataset.data,
                                      dataset.target,
                                      test_size=test_size,
@@ -36,85 +38,52 @@ def use_naive_bayes(training_data: tuple) -> dict:
     naive_bayes_models = [GaussianNB(), MultinomialNB(), BernoulliNB(), ComplementNB()]
     scores = {}
     for algo in naive_bayes_models:
-        print(f'using {str(algo)[:-2]}', end=': ')
+        algo_type = str(algo)[:-2]
+        print(f'using {algo_type}', end=': ')
         naive_model = algo
         naive_model.fit(X_train, y_train)
         y_pred = naive_model.predict(X_test)
         print(f'mislabeled points: {np.array((y_test != y_pred)).sum()} out of {X_test.shape[0]}')
         scores.update(
-            {algo: naive_model.score(X_test, y_test)}
+            {algo_type: naive_model.score(X_test, y_test)}
         )
     return scores
 
 
 def use_decision_tree(training_data: tuple,
                       raw_data: Bunch,
-                      tree_graph_filename: str = 'DtreeGraph'):
+                      tree_graph_filename: str = 'DtreeGraph',
+                      plot_and_save: bool = True) -> float:
 
     output_filename = './dtree_graphs/' + tree_graph_filename
     X_train, X_test, y_train, y_test = training_data
-    clf = tree.DecisionTreeClassifier(criterion='gini', max_depth=3)
+    clf = tree.DecisionTreeClassifier(criterion='gini',
+                                      max_depth=3)
     clf = clf.fit(X_train, y_train)
     score = clf.score(X_test, y_test)
     print('Tree score:', score)
 
     # plotting and saving the tree structure and info
-    tree.plot_tree(clf)     # this if you want just a simple plot without saving it to file.
-    dot_data = tree.export_graphviz(clf,
-                                    out_file=None,
-                                    feature_names=training_data[0].columns,
-                                    class_names=raw_data.target_names,
-                                    filled=True,
-                                    rounded=True,
-                                    special_characters=True,
-                                    leaves_parallel=True,
-                                    impurity=True)
-    graph = graphviz.Source(dot_data)
-    graph.render(output_filename)
+    if plot_and_save:
+        tree.plot_tree(clf)     # this if you want just a simple plot without saving it to file.
+        dot_data = tree.export_graphviz(clf,
+                                        out_file=None,
+                                        feature_names=training_data[0].columns,
+                                        class_names=raw_data.target_names,
+                                        filled=True,
+                                        rounded=True,
+                                        special_characters=True,
+                                        leaves_parallel=True,
+                                        impurity=True)
+        graph = graphviz.Source(dot_data)
+        graph.render(output_filename)
     return score
 
 
-def use_svm(c):
-    clf = svm.SVC(C=c)
-    clf.fit(X_train, y_train)
-    clf.predict(X_train)
-    return clf.score(X_test, y_test)
+def use_support_vector_machine(training_data: tuple) -> float:
 
-
-def use_support_vector_machines():
-    results = dict()
-    for c in range(1, 10):
-        results[c/10] = use_svm(c=c/10)
-    for c in range(1, 10):
-        results[c] = use_svm(c=c)
-
-    plt.xscale('log')
-    plt.plot(list(results.keys()), list(results.values()))
-
-    sv = clf.support_vectors_
-    svi = clf.support_
-    svc = clf.n_support_
-
-    clf = svm.SVC(
-            C=1.0,
-            kernel='rbf',
-            degree=3,
-            gamma='scale',
-            coef0=0.0,
-            shrinking=True,
-            probability=False,
-            tol=0.001,
-            cache_size=200,
-            class_weight=None,
-            verbose=False,
-            max_iter=- 1,
-            decision_function_shape='ovr',
-            break_ties=False,
-            random_state=None
-            )
-
-
-    clf = svm.LinearSVC(    # this works better then the other two.
+    X_train, X_test, y_train, y_test = training_data
+    clf = svm.LinearSVC(    # this works better than the other two.
         C=1.0,
         penalty='l2',
         loss='squared_hinge',
@@ -126,35 +95,32 @@ def use_support_vector_machines():
         class_weight=None,
         verbose=0,
         random_state=None,
-        max_iter=1000
+        max_iter=2000
     )
     clf.fit(X_train, y_train)
-    clf.predict(X_train)
-    clf.score(X_test, y_test)
-
-    clf = svm.NuSVC(nu=0.5,
-                    kernel='rbf',
-                    degree=3,
-                    gamma='scale',
-                    coef0=0.0,
-                    shrinking=True,
-                    probability=False,
-                    tol=0.001,
-                    cache_size=200,
-                    class_weight=None,
-                    verbose=False,
-                    max_iter=- 1,
-                    decision_function_shape='ovr',
-                    break_ties=False,
-                    random_state=None)
-    clf.fit(X_train, y_train)
-    clf.predict(X_train)
-    clf.score(X_test, y_test)
+    score = clf.score(X_test, y_test)
+    print(score)
+    return score
 
 
 if __name__ == '__main__':
-    use_case_dataset = visualise_sample_dataset('wine')
-    use_case_training_data = prepare_trainig_data(use_case_dataset)
-    naive_bayes_result = use_naive_bayes(training_data=use_case_training_data)
-    dtree_result = use_decision_tree(training_data=use_case_training_data,
-                                     raw_data=use_case_dataset)
+    results_list = []
+    for sample_dataset_name in DATASET_NAMES[:-1]:
+        use_case_dataset = load_sample_dataset(sample_dataset_name)
+        use_case_training_data = prepare_trainig_data(use_case_dataset)
+        print(f'Considering {sample_dataset_name}')
+        naive_bayes_result = use_naive_bayes(training_data=use_case_training_data)
+        dtree_result = use_decision_tree(training_data=use_case_training_data,
+                                         raw_data=use_case_dataset,
+                                         plot_and_save=False)
+        svm_result = use_support_vector_machine(training_data=use_case_training_data)
+        results_list.append(
+            dict(
+                {
+                    'dataset': sample_dataset_name,
+                    'svm': svm_result,
+                    'decision tree': dtree_result
+                },
+                **naive_bayes_result
+            )
+        )
