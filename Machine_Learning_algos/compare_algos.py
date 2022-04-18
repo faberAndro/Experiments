71198675e5ed2f6cd8279885cbca5365080ffa08
FabIT <40968674+faberAndro@users.noreply.github.com>
2022-04-18 21:46:54 +0100

    distinguished among classification and regression problems in compare algos

--- Machine_Learning_algos/compare_algos.py
+++ Machine_Learning_algos/compare_algos.py
@@ -1,11 +1,15 @@
 import graphviz
 import numpy as np
+import pandas as pd
 from sklearn import datasets, svm, tree
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import GaussianNB, MultinomialNB, BernoulliNB, ComplementNB
 from sklearn.utils import Bunch
 
-DATASET_NAMES = ['wine', 'iris', 'digits', 'diabetes', 'breast cancer', 'linnerud']
+COMPLETE_DATASET_NAMES = ['wine', 'iris', 'digits', 'breast cancer', 'diabetes', 'linnerud']
+CLASSIFICATION_TYPE_DATASETS = ['wine', 'iris', 'digits', 'breast cancer']
+MULTI_OUPUT_REGRESSION_TYPE_DATASETS = ['linnerud']
+REGRESSION_TYPE_DATASETS = ['diabetes']
 
 
 def load_sample_dataset(dataset_name: str) -> Bunch:
@@ -16,7 +20,7 @@ def load_sample_dataset(dataset_name: str) -> Bunch:
         dataset = datasets.load_iris(as_frame=True)
     elif dataset_name == 'digits':
         dataset = datasets.load_digits(as_frame=True)
-    elif dataset_name == 'brest cancer':
+    elif dataset_name == 'breast cancer':
         dataset = datasets.load_breast_cancer(as_frame=True)
     elif dataset_name == 'diabetes':
         dataset = datasets.load_diabetes(as_frame=True)
@@ -33,7 +37,9 @@ def prepare_trainig_data(dataset, test_size: float = 0.3) -> tuple:
     return training_data
 
 
-def use_naive_bayes(training_data: tuple) -> dict:
+def use_naive_bayes(training_data: tuple,
+                    problem: str = 'classification') -> dict:
+    # if it's a regression problem, Naive Bayes will be replaced by Logistic Regression
     X_train, X_test, y_train, y_test = training_data
     naive_bayes_models = [GaussianNB(), MultinomialNB(), BernoulliNB(), ComplementNB()]
     scores = {}
@@ -52,13 +58,18 @@ def use_naive_bayes(training_data: tuple) -> dict:
 
 def use_decision_tree(training_data: tuple,
                       raw_data: Bunch,
+                      problem: str = 'classification',
                       tree_graph_filename: str = 'DtreeGraph',
                       plot_and_save: bool = True) -> float:
 
     output_filename = './dtree_graphs/' + tree_graph_filename
     X_train, X_test, y_train, y_test = training_data
-    clf = tree.DecisionTreeClassifier(criterion='gini',
-                                      max_depth=3)
+    if problem == 'classification':
+        clf = tree.DecisionTreeClassifier(criterion='gini',
+                                          max_depth=3)
+    else:
+        clf = tree.DecisionTreeRegressor(criterion='squared_error',
+                                         max_depth=3)
     clf = clf.fit(X_train, y_train)
     score = clf.score(X_test, y_test)
     print('Tree score:', score)
@@ -66,10 +77,11 @@ def use_decision_tree(training_data: tuple,
     # plotting and saving the tree structure and info
     if plot_and_save:
         tree.plot_tree(clf)     # this if you want just a simple plot without saving it to file.
+        class_names = raw_data.target_names if problem == 'classification' else None
         dot_data = tree.export_graphviz(clf,
                                         out_file=None,
                                         feature_names=training_data[0].columns,
-                                        class_names=raw_data.target_names,
+                                        class_names=class_names,
                                         filled=True,
                                         rounded=True,
                                         special_characters=True,
@@ -80,23 +92,27 @@ def use_decision_tree(training_data: tuple,
     return score
 
 
-def use_support_vector_machine(training_data: tuple) -> float:
+def use_support_vector_machine(training_data: tuple,
+                               problem: str = 'classification') -> float:
 
     X_train, X_test, y_train, y_test = training_data
-    clf = svm.LinearSVC(    # this works better than the other two.
-        C=1.0,
-        penalty='l2',
-        loss='squared_hinge',
-        dual=True,
-        tol=0.0001,
-        multi_class='ovr',
-        fit_intercept=True,
-        intercept_scaling=1,
-        class_weight=None,
-        verbose=0,
-        random_state=None,
-        max_iter=2000
-    )
+    if problem == 'classification':
+        clf = svm.LinearSVC(    # this works better than the other two.
+            C=1.0,
+            penalty='l2',
+            loss='squared_hinge',
+            dual=True,
+            tol=0.0001,
+            multi_class='ovr',
+            fit_intercept=True,
+            intercept_scaling=1,
+            class_weight=None,
+            verbose=0,
+            random_state=None,
+            max_iter=2000
+        )
+    else:
+        clf = svm.LinearSVR()
     clf.fit(X_train, y_train)
     score = clf.score(X_test, y_test)
     print(score)
@@ -104,16 +120,28 @@ def use_support_vector_machine(training_data: tuple) -> float:
 
 
 if __name__ == '__main__':
+    PROBLEM_TYPE = 'regression'
+
+    if PROBLEM_TYPE == 'classification':
+        dataset_names = CLASSIFICATION_TYPE_DATASETS
+    else:
+        dataset_names = REGRESSION_TYPE_DATASETS
     results_list = []
-    for sample_dataset_name in DATASET_NAMES[:-1]:
+    for sample_dataset_name in dataset_names:
         use_case_dataset = load_sample_dataset(sample_dataset_name)
         use_case_training_data = prepare_trainig_data(use_case_dataset)
         print(f'Considering {sample_dataset_name}')
-        naive_bayes_result = use_naive_bayes(training_data=use_case_training_data)
+        if PROBLEM_TYPE == 'classification':
+            naive_bayes_result = use_naive_bayes(training_data=use_case_training_data,
+                                                 problem=PROBLEM_TYPE)
+        else:
+            naive_bayes_result = {}
         dtree_result = use_decision_tree(training_data=use_case_training_data,
                                          raw_data=use_case_dataset,
-                                         plot_and_save=False)
-        svm_result = use_support_vector_machine(training_data=use_case_training_data)
+                                         plot_and_save=True,
+                                         problem=PROBLEM_TYPE)
+        svm_result = use_support_vector_machine(training_data=use_case_training_data,
+                                                problem=PROBLEM_TYPE)
         results_list.append(
             dict(
                 {
@@ -124,3 +152,4 @@ if __name__ == '__main__':
                 **naive_bayes_result
             )
         )
+    comparison = pd.DataFrame.from_records(data=results_list)
